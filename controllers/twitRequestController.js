c33f09559420d218e81fa926f45904bfe2c76c56
jverfr1 <jverfr1@gmail.com>
2016-12-23 01:24:07 +0100

    Code refactored

--- controllers/twitRequestController.js
+++ controllers/twitRequestController.js
@@ -3,18 +3,10 @@ var Scrape = require('../scrape');
 function getUserInfo(req,res) {
 	if (req.params.userId) {
 		var userId = req.params.userId;
-	}
-
-	Scrape.getInfo(res,userId);
-	
-
-}
-
-function getUser(req) {
-	return req.params.userId;
+	};
+	Scrape.getInfo(res,userId);	
 }
 
 module.exports = {
 	getUserInfo,
-	getUser
 }
